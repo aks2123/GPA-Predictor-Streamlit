06ebaa2d5b13a6a53ed278b1cb6b43bae7152b77
aks2123 <drach4@gmail.com>
2023-08-28 14:11:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ import streamlit as st
 from prediction import predict
 import numpy as np
 
-st.title ('GPA predictor from SAT')
-st.write ('Hello World!')
-r = st.radio ('Select GPA', (1000, 5000, 1000))
-if r == 1000:
-    st.write ('Nice one')
-else:
-    st.write ('try again')
+st.title ('GPA predictor from SAT Score')
+st.write ('Please select SAT score and press the *Predict GPA* button')
 
-st.slider ('select', min_value= 10, max_value= 100)
 
 sat = st.number_input ('select', min_value= 400, max_value= 1600)
 
