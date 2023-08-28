2f9262e0c1112b3d3d34486f8854215822217ef7
aks2123 <drach4@gmail.com>
2023-08-28 14:13:48 -0500

    style change

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from prediction import predict
 import numpy as np
 
 st.title ('GPA predictor from SAT Score')
-st.write ('Please select SAT score and press the *Predict GPA* button')
+st.write ('Please select SAT score and press the ***Predict GPA*** button')
 
 
 sat = st.number_input ('select', min_value= 400, max_value= 1600)
 
 if st.button ('Predict GPA'):
     GPA = predict ([[sat]])
-    st.write ('GPA IS', GPA)
+    st.write ('GPA would be', GPA)
     
